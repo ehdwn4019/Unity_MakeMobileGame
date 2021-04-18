1c2eb337b60b4649c76f8dbf26bd12394e7f4159
ehdwn4019 <ehdwn7531@gmail.com>
2021-04-19 01:18:45 +0900

    - Chap05 - Animation 다루기

---
+++ Chap05/Assets/Scripts/AttackCtrl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCtrl : MonoBehaviour
+{
+    protected Animator mAvatar;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAvatar = GetComponent<Animator>();
+        
+    }
+
+    public void OnAttackBtn()
+    {
+        mAvatar.SetTrigger("ATTACK");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
